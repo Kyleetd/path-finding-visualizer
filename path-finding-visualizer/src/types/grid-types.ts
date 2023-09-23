d222e427157140dcedc825b443e5e79e2be5e387
Kylee Dyck <kyleetd@gmail.com>
2023-09-23 13:40:23 -0700

    Fix linter

--- path-finding-visualizer/src/types/grid-types.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export type CellTypes = 'start' | 'end' | 'wall' | 'empty';
-
-export interface CellObject {
-  type: CellTypes;
-  cell: JSX.Element;
-  key: uuidv4;
-}
